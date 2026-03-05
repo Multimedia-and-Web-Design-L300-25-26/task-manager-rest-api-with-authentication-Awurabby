9094f8ac05a142335025c09b92966beb32784a7a
EwuraAbena Antwi <ewuraabenaantwibosiako@gmail.com>
2026-03-05 14:42:16 +0000

    define user and task models

--- src/models/User.js
+++ src/models/User.js
@@ -11,7 +11,23 @@ import mongoose from "mongoose";
 
 
 const userSchema = new mongoose.Schema({
-  // Students implement
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  createdAt: {
+    default: Date.now
+  }
 });
 
 const User = mongoose.model("User", userSchema);
